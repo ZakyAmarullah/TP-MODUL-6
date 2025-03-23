16598ac2684c3b7a7dabe819f886c3b1f2ada999
ZakyAmarullah <zakym0208@gmail.com>
2025-03-23 23:29:51 +0700

    Implementasi desain by contract

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 public class SayaTubeVideo
 {
@@ -8,6 +9,10 @@ public class SayaTubeVideo
 
     public SayaTubeVideo(string title)
     {
+        if(string.IsNullOrEmpty(title) || title.Length > 100)
+        {
+            throw new ArgumentException("Judul tidak boleh NULL (kosong) dan maksimal hanya 100 karakter!");
+        }
         Random random = new Random();
         this.id = random.Next(10000, 99999);
         this.title = title;
@@ -16,7 +21,22 @@ public class SayaTubeVideo
 
     public void IncreasePlayCount(int count)
     {
-        playCount += count;
+        if(count > 10000000 || count < 0)
+        {
+            throw new ArgumentException("Input play count maksimal 10.000.000 dan tidak boleh bernilai negatif!");
+        }
+
+        try
+        {
+            checked
+            {
+                playCount += count;
+            }
+        }
+        catch(OverflowException e)
+        {
+            Console.WriteLine("Error: terjadi over flow saat melakukan play count! " + e.Message);
+        }
     }
 
     public void PrintVideoDetails()
@@ -31,8 +51,18 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Muhammad Zaky Amarullah");
-        video.IncreasePlayCount(10);
-        video.PrintVideoDetails();
+        try
+        {
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Muhammad Zaky Amarullah");
+            for(int i = 0; i < 10; i++)
+            {
+                video.IncreasePlayCount(10000000);
+            }
+            video.PrintVideoDetails();
+        }
+        catch(ArgumentException e)
+        {
+            Console.WriteLine("ERROR: " + e.Message);
+        }
     }
 }

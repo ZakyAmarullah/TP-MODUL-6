c149e50d6e6bc5977d2b6163781a343bf0420e15
ZakyAmarullah <zakym0208@gmail.com>
2025-03-23 23:14:28 +0700

    menambahkan class SayaTubeVideo

--- obj/Debug/net8.0/tpmodul6_103022300045.AssemblyInfo.cs
+++ obj/Debug/net8.0/tpmodul6_103022300045.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul6_103022300045")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e988fd427ed4aa2b17da8261de20460038c7490b")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul6_103022300045")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul6_103022300045")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
